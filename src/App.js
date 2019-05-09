cc6e8e2838622adb1526c5b732aee751ff420e76
binsera <binsera001@gmail.com>
2019-05-09 11:13:41 +0100

    displayed login and register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 
 import './App.css';
+import Login from './Components/Login';
+import Register from './Components/Register';
 
 function App() {
   return (
     <div className="App">
-      
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <Login/>
+          </div>
+          <div class="col-lg-6">
+            <Register/>
+            </div>
+  
+        </div>
+      </div>
     </div>
   );
 }
